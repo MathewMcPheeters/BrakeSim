b71b3edd46468270d488ac785d2e82a445782445
John Mohr <jomohr@unm.edu>
2018-02-08 21:50:40 -0700

    Committed Interfaces.java and Gear.java for the backend of the project.

---
+++ src/Gear.java
@@ -0,0 +1,7 @@
+public enum Gear
+{
+  PARK,
+  REVERSE,
+  NEUTRAL,
+  DRIVE
+}
---
+++ src/Interfaces.java
@@ -0,0 +1,80 @@
+
+public class Interfaces
+{
+  private int topSpeed;
+  private Gear gear;
+  private int currentSpeed;
+  private int acceleration;
+  private int brakingPower;
+  private boolean braking;
+
+  public Interfaces(int topSpeed)
+  {
+    this.topSpeed = topSpeed;
+  }
+
+  public void setTopSpeed(int topSpeed)
+  {
+    this.topSpeed = topSpeed;
+  }
+  public int getTopSpeed()
+  {
+    return topSpeed;
+  }
+
+  public void setGear(Gear gear)
+  {
+    this.gear = gear;
+  }
+  public Gear getGear()
+  {
+    return gear;
+  }
+
+  public void setCurrentSpeed(int newSpeed)
+  {
+    if(newSpeed > topSpeed)
+    {
+      this.currentSpeed = topSpeed;
+    }
+    else
+    {
+      this.currentSpeed = newSpeed
+    }
+  }
+  public int getCurrentSpeed()
+  {
+    return currentSpeed;
+  }
+
+  public void setAcceleration(int acceleration)
+  {
+    this.acceleration = acceleration;
+  }
+  public int getAcceleration()
+  {
+    return acceleration;
+  }
+
+  public void setBrakingPower(int brakingPower)
+  {
+    this.brakingPower = brakingPower;
+  }
+  public int getBrakingPower()
+  {
+    return brakingPower;
+  }
+
+  public void setBraking(boolean braking)
+  {
+    this.braking = braking;
+    if(braking == true)
+    {
+      acceleration = 0;
+    }
+  }
+  public boolean getBraking()
+  {
+    return braking;
+  }
+}
