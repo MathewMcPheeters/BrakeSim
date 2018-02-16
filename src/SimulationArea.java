929f2f9467fa0cc48a6dec036cc8c25baf1c9f76
jkrukar <john.a.krukar@gmail.com>
2018-02-15 21:59:59 -0700

    Added a basic background image to experiment with.
    Added a single road dash and added a basic translate transition.
    
    Next steps: I plan on using a series of road dashes and/or other objects (trees,flags, etc?) to give the illusion of speed during simulation.

--- src/SimulationArea.java
+++ src/SimulationArea.java
@@ -1,9 +1,35 @@
-import javafx.scene.layout.Pane;
+import javafx.animation.TranslateTransition;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 
-/** 
-  * Simulation area will be defined in here. Note that this class extends Pane, so we can add
-  * a SimulationArea to a Scene just as we would add a Pane.
-  */
+import javafx.scene.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+
+/**
+ * Simulation area will be defined in here. Note that this class extends Pane, so we can add
+ * a SimulationArea to a Scene just as we would add a Pane.
+ */
 public class SimulationArea extends Pane {
+
+    Image backgroundImageFile = new Image("Resources/testBackground.png");
+    BackgroundImage backgroundImage = new BackgroundImage(backgroundImageFile, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+
+    Rectangle roadDash = new Rectangle(200,420,40,10);
+    TranslateTransition dashTranslate = new TranslateTransition(Duration.millis(2000), roadDash);
+
+
+    public SimulationArea(){
+
+        this.setBackground(new Background(backgroundImage));
+        this.roadDash.setFill(Color.WHITE);
+        dashTranslate.setByX(-50);
+        this.getChildren().add(roadDash);
+        dashTranslate.play();
+
+    }
+
+
 
 }
