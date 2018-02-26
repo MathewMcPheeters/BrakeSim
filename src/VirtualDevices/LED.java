df8b80f8cd886d236a924e91c1877db07eb7940b
James <jp55674@unm.edu>
2018-02-25 17:16:34 -0700

    fixed typo

--- src/VirtualDevices/LED.java
+++ src/VirtualDevices/LED.java
@@ -3,7 +3,7 @@ package VirtualDevices;
  * James Perry
  * CS 460
  * This class contains the LED virtual device. It can be used to set the color
- * of the LED light on the Emergency Brake Button.
+ * of the LED light on the Emergency Brake Button by calling setColor();
  *
  */
 public class LED
@@ -15,7 +15,7 @@ public class LED
     BLUE
   };
   private Color color;
-  public void setState(Color color)
+  public void light(Color color)
   {
     this.color = color;
   }
